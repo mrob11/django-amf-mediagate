4b0cc6ea1f377af18ab57d84b044ab9531d6fc5f
Mike Robinson <mike.robinson.81@gmail.com>
2012-01-08 15:50:50 -0500

    bug fix in urls file

--- mediagate/urls.py
+++ mediagate/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns
 
-urlpatterns += patterns('mediagate.amfgateway', 
+urlpatterns = patterns('mediagate.amfgateway', 
     # AMF Gateway
     (r'^amf/', 'mediagateGateway'),
 )
